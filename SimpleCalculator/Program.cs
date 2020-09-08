64f0af8ad7b837cd61affcdbd4348cd00bfbecb5
Andrian <Andrian@ANDRIAN>
2020-09-07 20:40:44 -0400

    added hello word for testing

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -21,17 +21,30 @@ namespace SimpleCalculator
                 CalculatorEngine calculatorEngine = new CalculatorEngine();
 
                 double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+
+                
+
+
                 double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
                 string operation = Console.ReadLine();
 
+
+               
+
                 double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+                Console.WriteLine("hello");
 
                 Console.WriteLine(result);
 
-            } catch (Exception ex)
+
+
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+
+           
         }
     }
 }
